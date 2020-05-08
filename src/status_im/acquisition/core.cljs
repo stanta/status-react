b70d8c0e3846ba881a4ed9d16a731d58377c067c
Gheorghe Pinzaru <feross95@gmail.com>
2020-05-08 18:08:53 +0300

    Add acquisition backend methods

---
+++ src/status_im/acquisition/core.cljs
@@ -0,0 +1,43 @@
+(ns status-im.acquisition.core
+  (:require [re-frame.core :as re-frame]
+            [status-im.utils.fx :as fx]
+            [status-im.ethereum.json-rpc :as json-rpc]
+            [status-im.waku.core :as waku]
+            [taoensso.timbre :as log]
+            [status-im.ethereum.core :as ethereum]
+            [status-im.utils.types :as types]
+            [status-im.utils.build :as build]))
+
+(fx/defn handle-error
+  {:events [::on-error]}
+  [_ error]
+  (prn error)
+  {:utils/show-popup {:title   "Request failed"
+                      :content (str error)}})
+
+(fx/defn handle-acquisition
+  {:events [::handle-acquisition]}
+  [{:keys [db] :as cofx} {:keys [message on-success]}]
+  (let [msg (types/clj->json message)]
+    {::json-rpc/call [{:method     (json-rpc/call-ext-method (waku/enabled? cofx) "signMessageWithChatKey")
+                       :params     [msg]
+                       :on-error   #(re-frame/dispatch [::on-error "Could not sign message"])
+                       :on-success #(re-frame/dispatch [::call-acquisition-gateway
+                                                        {:chat-key   (get-in db [:multiaccount :public-key])
+                                                         :message    msg
+                                                         :on-success on-success} %])}]}))
+
+(fx/defn call-acquisition-gateway
+  {:events [::call-acquisition-gateway]}
+  [cofx {:keys [chat-key message on-success]} sig]
+  (let [payload {:chat_key chat-key
+                 :msg      message
+                 :sig      sig
+                 :version  2}]
+    {:http-post {:url                   (get-in cofx [:db :acquisition-gateway])
+                 :opts                  {:headers {"Content-Type" "application/json"}}
+                 :data                  (types/clj->json payload)
+                 :success-event-creator (fn [response]
+                                          [on-success (types/json->clj (get response :response-body))])
+                 :failure-event-creator (fn [error]
+                                          [::on-error (types/json->clj error)])}}))
