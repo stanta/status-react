b54f6f2a9a68848665a3ccda335e1e731bb61578
Gheorghe Pinzaru <feross95@gmail.com>
2020-07-10 11:55:18 +0300

    Add message reactions UI

--- src/status_im/ui/screens/chat/message/reactions.cljs
+++ src/status_im/ui/screens/chat/message/reactions.cljs
@@ -2,6 +2,7 @@
   (:require [status-im.react-native.resources :as resources]
             [cljs-bean.core :as bean]
             [status-im.i18n :as i18n]
+            [status-im.ui.screens.chat.styles.photos :as photos]
             [status-im.constants :as constants]
             [reagent.core :as reagent]
             [quo.react-native :as rn]
@@ -20,12 +21,12 @@
 (def translate-x   27)
 (def translate-y   -24)
 
-(def reactions [[constants/emoji-reaction-love (:love resources/reactions)]
-                [constants/emoji-reaction-thumbs-up (:thumbs-up resources/reactions)]
-                [constants/emoji-reaction-thumbs-down (:thumbs-down resources/reactions)]
-                [constants/emoji-reaction-laugh (:laugh resources/reactions)]
-                [constants/emoji-reaction-sad (:sad resources/reactions)]
-                [constants/emoji-reaction-angry (:angry resources/reactions)]])
+(def reactions {constants/emoji-reaction-love        (:love resources/reactions)
+                constants/emoji-reaction-thumbs-up   (:thumbs-up resources/reactions)
+                constants/emoji-reaction-thumbs-down (:thumbs-down resources/reactions)
+                constants/emoji-reaction-laugh       (:laugh resources/reactions)
+                constants/emoji-reaction-sad         (:sad resources/reactions)
+                constants/emoji-reaction-angry       (:angry resources/reactions)})
 
 (defn measure-in-window [ref cb]
   (.measureInWindow ^js ref cb))
@@ -60,7 +61,7 @@
            {:border-top-right-radius 4}
            {:border-top-left-radius 4})))
 
-(defn reactions-row []
+(defn reactions-picker-row []
   {:flex-direction     :row
    :pading-vertical    6
    :padding-horizontal 7})
@@ -73,7 +74,7 @@
 
 (defn picker [{:keys [outgoing on-copy on-reply send-emoji]}]
   [animated/view {:style (container-style {:outgoing outgoing})}
-   [rn/view {:style (reactions-row)}
+   [rn/view {:style (reactions-picker-row)}
     (for [[id resource] reactions]
       ^{:key id}
       [rn/touchable-opacity {:on-press #(send-emoji id)
@@ -89,6 +90,55 @@
     [quo/button {:type     :secondary
                  :on-press on-copy}
      (i18n/label :t/sharing-copy-to-clipboard)]]])
+
+(defn reaction-style [{:keys [outgoing own]}]
+  (merge {:border-top-left-radius     10
+          :border-top-right-radius    10
+          :border-bottom-right-radius 10
+          :border-bottom-left-radius  10
+          :flex-direction             :row
+          :margin-vertical            2
+          :padding-right              8
+          :padding-left               2
+          :padding-vertical           2}
+         (if own
+           {:background-color (:interactive-01 @colors/theme)}
+           {:background-color (:interactive-02 @colors/theme)})
+         (if outgoing
+           {:border-top-right-radius 2
+            :margin-left             4}
+           {:border-top-left-radius 2
+            :margin-right           4})))
+
+(defn reaction-quantity-style [{:keys [own]}]
+  {:font-size   12
+   :line-height 16
+   :color       (if own
+                  (:text-05 @colors/theme)
+                  (:text-01 @colors/theme))})
+
+(defn reactions-row [{:keys [outgoing]}]
+  {:flex-direction  :row
+   :justify-content (if outgoing :flex-end :flex-start)
+   :padding-right   8
+   :padding-left    (+ 16 photos/default-size)})
+
+(defn reaction [{:keys [outgoing]} {:keys [own emoji-id quantity]}]
+  [rn/view {:style (reaction-style {:outgoing outgoing
+                                    :own      own})}
+   [rn/image {:source (get reactions emoji-id)
+              :style  {:width        16
+                       :height       16
+                       :margin-right 4}}]
+   [quo/text {:style (reaction-quantity-style {:own own})}
+    quantity]])
+
+(defn message-reactions [message reactions]
+  (when (pos? (count reactions))
+    [rn/view {:style (reactions-row message)}
+     (for [emoji-reaction reactions]
+       ^{:key (str emoji-reaction)}
+       [reaction message emoji-reaction])]))
 
 (def picker-modal
   (reagent/adapt-react-class
